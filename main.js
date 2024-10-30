7e93f5e6a49a9d870be43574c6bdcdc6921b600b
Aditya Kumar Gupta <78353347+adityaguptareal@users.noreply.github.com>
2024-10-30 22:51:51 +0530

    Added new packages

--- main.js
+++ main.js
@@ -1,23 +1,28 @@
-const express=require("express")
-const app=express()
-const port=3000
-const {userRouter}=require("./Routes/userRoutes")
-const {adminRouter}=require("./Routes/adminRoutes")
+const express = require("express")
+const app = express()
+const port = 3000
+const { userRouter } = require("./Routes/userRoutes")
+const { adminRouter } = require("./Routes/adminRoutes")
 const mongoose = require("mongoose")
-const {mongooseURL}=require("./config")
+const dotenv = require('dotenv')
+const MONGODB_URL = process.env.MONGODB_URL
 app.use(express.json())
 
-app.use("/api/v1/user",userRouter)
-app.use("/api/v1/admin",adminRouter)
+app.use("/api/v1/user", userRouter)
+app.use("/api/v1/admin", adminRouter)
 async function main() {
     try {
-        await mongoose.connect(mongooseURL)
+        await mongoose.connect(MONGODB_URL)
     } catch (error) {
-        res.status(403).json({msg:"Error while connecting"})
+        console.log(error);
+
     }
-    app.listen(port,()=>{
+    app.listen(port, () => {
         console.log('App is running at port', port);
+        console.log(MONGODB_URL);
         
+
     })
+
 }
 main()
