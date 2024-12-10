255f363ea45c018e6d6a7fe12f4b0d54f6c012af
Aditya Kumar Gupta <78353347+adityaguptareal@users.noreply.github.com>
2024-12-10 23:55:34 +0530

    Created a Team Member Card Component

---
+++ Frontend/src/Components/TeamMemberCard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function TeamMemberCard() {
+  return (
+    <div className='h-[300px] w-[250px] bg-white rounded-md text-gray-700 flex justify-center items-center'>
+      
+    </div>
+  )
+}
+
+export default TeamMemberCard
