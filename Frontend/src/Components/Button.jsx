c01d09282ab9b0dd68ad0a1329713dc07e562192
Aditya Kumar Gupta <78353347+adityaguptareal@users.noreply.github.com>
2024-12-24 20:22:55 +0530

    updatd required pages

--- Frontend/src/Components/Button.jsx
+++ Frontend/src/Components/Button.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Button = ({ text,rounded }) => {
   return (
     <button
-      className={`  px-2 MobileM:px-5 sm:px-6 py-2 text-white font-normal rounded-full font-Montserrat hover:scale-[1.09] transition-all`}
+      className={`  px-3 MobileM:px-5 sm:px-6 py-2 text-white font-normal rounded-full font-Montserrat hover:scale-[1.09] transition-all`}
       style={{
         background: 'linear-gradient(90deg, rgba(148,0,252,1) 0%, rgba(0,142,247,1) 100%, rgba(31,31,31,1) 100%)',
       }}
