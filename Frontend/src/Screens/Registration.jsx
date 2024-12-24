670546b425dc4df66f781b999a8fc4ce3bf5eda4
Aditya Kumar Gupta <78353347+adityaguptareal@users.noreply.github.com>
2024-12-24 19:37:54 +0530

    Adding Registration page

---
+++ Frontend/src/Screens/Registration.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { useForm } from 'react-hook-form';
+import Navbar from '../Components/Navbar';
+function Registration() {
+    return (
+        <>
+            <div id='page' className='h-screen w-screen bg-gray-950'>
+                <Navbar />
+                
+
+            </div>
+        </>
+    )
+
+}
+
+export default Registration;
