65dbaf874f6d4b2b992211e9480a4ad258a9b6a8
Aditya Kumar Gupta <78353347+adityaguptareal@users.noreply.github.com>
2024-12-27 01:06:38 +0530

    Updated the animation and assets

--- Frontend/src/AssetsExport.js
+++ Frontend/src/AssetsExport.js
@@ -1,16 +1,31 @@
-import HeroImage from "./assets/Landingpage/ARVR.jpg"
-import VytoflowGroup from "./assets/Landingpage/VytoflowGroup.jpg"
-import CollegeAuditoriam from "./assets/Landingpage/CollegeAuditoriam.jpg"
-import Cultural from "./assets/Landingpage/Cultural.png"
-import Hackathon from "./assets/Landingpage/Hackathon.png"
-import ArtistryArena from "./assets/Landingpage/Artistry Arena.png"
-import Quizathon from "./assets/Landingpage/Quizathon.png"
-import Esports from "./assets/Landingpage/Esports.png"
-import VytFlowTeam from "./assets/Landingpage/VytoFlowTeamStage.jpg"
-import VytoFlowSpecialGuest from "./assets/Landingpage/VytoFlowSpecialGuest.jpg"
-import Specialguest from "./assets/Landingpage/Specialgues3.jpg"
-import SpecialGuestPerformance from "./assets/Landingpage/SpecialGuestPerformance.jpg"
-import Vytfolowteaminground from "./assets/Landingpage/Vytfolowteaminground.jpg"
+import HeroImage from "./assets/Landingpage/ExploreEventsImage/ARVR.jpg"
+import VytoflowGroup from "./assets/Landingpage/ExploreEventsImage/VytoflowGroup.jpg"
+import CollegeAuditoriam from "./assets/Landingpage/ExploreEventsImage/CollegeAuditoriam.jpg"
+import Cultural from "./assets/Landingpage/ExploreEventsImage/Cultural.png"
+import Hackathon from "./assets/Landingpage/ExploreEventsImage/Hackathon.png"
+import ArtistryArena from "./assets/Landingpage/ExploreEventsImage/Artistry Arena.png"
+import Quizathon from "./assets/Landingpage/ExploreEventsImage/Quizathon.png"
+import Esports from "./assets/Landingpage/ExploreEventsImage/Esports.png"
+import VytFlowTeam from "./assets/Landingpage/ExploreEventsImage/VytoFlowTeamStage.jpg"
+import VytoFlowSpecialGuest from "./assets/Landingpage/ExploreEventsImage/VytoFlowSpecialGuest.jpg"
+import Specialguest from "./assets/Landingpage/ExploreEventsImage/Specialgues3.jpg"
+import SpecialGuestPerformance from "./assets/Landingpage/ExploreEventsImage/SpecialGuestPerformance.jpg"
+import Vytfolowteaminground from "./assets/Landingpage/ExploreEventsImage/Vytfolowteaminground.jpg"
+import AdityaSingh from "./assets/Landingpage/EventCorditnators/AdityaSingh.png"
+import Aditya from "./assets/Landingpage/EventCorditnators/Aditya.png"
+import Anshika from "./assets/Landingpage/EventCorditnators/Anshika.png"
+import Dhiya from "./assets/Landingpage/EventCorditnators/Dhiya.png"
+import Garvit from "./assets/Landingpage/EventCorditnators/Garvit.png"
+import Harsh from "./assets/Landingpage/EventCorditnators/Harsh.png"
+import Ishika from "./assets/Landingpage/EventCorditnators/Ishika.png"
+import Lavi from "./assets/Landingpage/EventCorditnators/Lavi.png"
+import Rahul from "./assets/Landingpage/EventCorditnators/Rahul.png"
+import Ritik from "./assets/Landingpage/EventCorditnators/Ritik.png"
+import Shubham from "./assets/Landingpage/EventCorditnators/Shubham.png"
+import Swati from "./assets/Landingpage/EventCorditnators/Swati.png"
+import Vaibhav from "./assets/Landingpage/EventCorditnators/Vaibhav.png"
+import Vanshika from "./assets/Landingpage/EventCorditnators/Vanshika.png"
+import Vishal from "./assets/Landingpage/EventCorditnators/Vishal.png"
 
 
 
@@ -29,7 +44,21 @@ const Images = {
     Esports,
     VytFlowTeam,
     VytoFlowSpecialGuest,
-
+    AdityaSingh,
+    Aditya,
+    Anshika,
+    Dhiya,
+    Garvit,
+    Harsh,
+    Ishika,
+    Lavi,
+    Rahul,
+    Ritik,
+    Shubham,
+    Swati,
+    Vaibhav,
+    Vanshika,
+    Vishal
 
 
 }
