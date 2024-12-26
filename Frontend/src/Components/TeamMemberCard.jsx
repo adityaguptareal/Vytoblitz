5f528a3a2f756e1f5e92b3067698c02fe3f70275
Aditya Kumar Gupta <78353347+adityaguptareal@users.noreply.github.com>
2024-12-27 01:04:45 +0530

    Created event coordinator card

--- Frontend/src/Components/TeamMemberCard.jsx
+++ Frontend/src/Components/TeamMemberCard.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
-
-function TeamMemberCard() {
+// import Images from '../AssetsExport'
+function TeamMemberCard({name, img}) {
   return (
-    <div className='h-[300px] w-[250px] bg-white rounded-md text-gray-700 flex justify-center items-center'>
-      
+    <div class="flex flex-col bg-white shadow-sm border border-slate-200 rounded-lg my-6 h-[250px] w-[200px]">
+    <div class="m-2.5 overflow-hidden rounded-md  flex justify-center items-center">
+      <img class="w-full h-full  object-cover " src={img} alt="profile-picture" />
     </div>
+    <div class="p-6 text-center">
+      <h4 class="mb-1 text-xl font-semibold text-slate-800">
+       {name}
+      </h4>
+
+    </div>
+  </div>
   )
 }
 
