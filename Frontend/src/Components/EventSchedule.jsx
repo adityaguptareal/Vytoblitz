277b9e07ca8eaa18f96275578881f810b2f1ae61
Aditya Kumar Gupta <78353347+adityaguptareal@users.noreply.github.com>
2024-12-29 13:14:45 +0530

    Added data in the component

--- Frontend/src/Components/EventSchedule.jsx
+++ Frontend/src/Components/EventSchedule.jsx
@@ -1,22 +1,75 @@
-import React from 'react'
+import { React, useState } from 'react'
 
- function EventSchedule({time,eventName,venue}) {
+function EventSchedule({ time, eventName, venue }) {
+    const [date, setDate] = useState('28 Nov');
+  
+
+    const eventData28Nov = [{
+        id: 1,
+        time: "10:00 AM",
+        date: "28 Nov",
+        eventName: "Algo Quizathon",
+        venue: "ITS Engineering LAB 4",
+    }, {
+        id: 2,
+        time: "11:00 AM",
+        date: "28 Nov",
+        eventName: "Artistry Arena",
+        venue: "TS Engineering LAB 3 ",
+    },
+    {
+        id: 3,
+        time: "12:00 AM",
+        date: "28 Nov",
+        eventName: "Esports Game",
+        venue: "ITS Engineering COE Room",
+    }, {
+        id: 4,
+        time: "9:00 AM",
+        date: "28 Nov",
+        eventName: "Vyto HackClash",
+        venue: "ITS Engineering COE Room ",
+    }];
+
+    const eventData29Nov = [{
+        id: 1,
+        time: "10:00 AM",
+        date: "29 Nov",
+        eventName: "Cultural Carnival",
+        venue: "ITS Engineering Sardar Patel Auditoriam",
+    }];
+
+    const finalDate = date === "28 Nov" ? eventData28Nov : eventData29Nov;
 
     return (
-        <div>
-            <div className='flex justify-between items-center border-t border-white py-4 font-Montserrat w-3/4 mx-auto text-center'>
-                <div className='text-base'>{time}</div>
-                <div id="event" className='flex gap-3 justify-center items-center'>
-                    <div className='text-lg'>{eventName}</div>
-                    <div id="badge" className='text-slate-950 text-sm px-2 font-semibold bg-white rounded-md '>
-                        In Person
-                    </div>
-                </div>
-                <div id="Venue">{venue}</div>
-                <button className='bg-transparent border border-white px-3 py-2'>View Details</button>
-            </div>
-        </div>
-    )
-}
 
-export default EventSchedule
+        <>
+            <div id='dateButton' className='flex gap-5 justify-center py-6'>
+                <button onClick={() => setDate('28 Nov')} className={`text-white font-Montserrat px-6 py-2 border ${date === '28 Nov' ? 'border-white' : 'border-transparent'}`}>28 Nov</button>
+                <button onClick={() => setDate('29 Nov')} className={`text-white font-Montserrat px-6 py-2 border ${date === '29 Nov' ? 'border-white' : 'border-transparent'}`}>29 Nov</button>
+            </div>
+            
+
+                {finalDate.map((eventData, index) => {
+                    return (
+                        <div key={index}>
+                            <div className='flex justify-between items-center border-t border-white py-4 font-Montserrat w-3/4 mx-auto text-center'>
+                                <div className='text-base'>{eventData.time}</div>
+                                <div id="event" className='flex gap-3 justify-center items-center'>
+                                    <div className='text-lg'>{eventData.eventName}</div>
+                                    <div id="badge" className='text-slate-950 text-sm px-2 font-semibold bg-white rounded-md '>
+                                        In Person
+                                    </div>
+                                </div>
+                                <div id="Venue">{eventData.venue}</div>
+                                <button className='bg-transparent border border-white px-3 py-2'>View Details</button>
+                            </div>
+                        </div>
+                    );
+                })}
+             
+            </>
+        )
+    }
+    
+    export default EventSchedule
