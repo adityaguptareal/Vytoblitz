88116d552c1b26d19ab006713c400aacc9fa46ca
Aditya Kumar Gupta <78353347+adityaguptareal@users.noreply.github.com>
2024-12-30 00:08:03 +0530

    Fixes flex properties for mobile

--- Frontend/src/Components/EventCta.jsx
+++ Frontend/src/Components/EventCta.jsx
@@ -7,10 +7,10 @@ function EventCta({ heading, description, primaryBtn, secondaryBtn, primaryBtnLi
         <div>
             <div  style={{
         background: 'linear-gradient(90deg, rgba(148,0,252,1) 0%, rgba(0,142,247,1) 100%, rgba(31,31,31,1) 100%)',
-      }} className='w-full py-16  font-Montserrat text-white flex items-center justify-center flex-col gap-5 flex-wrap'>
+      }} className='w-full py-10 md:py-16  font-Montserrat text-white flex items-center justify-center flex-col gap-5 flex-wrap'>
                 <div className='md:max-w-[650px] max-w-[300px] flex items-center justify-center flex-col gap-5'>
 
-                    <h1 className='text-4xl font-bold text-center '>{heading}</h1>
+                    <h1 className='md:text-4xl text-3xl font-bold text-center '>{heading}</h1>
                     <p className='text-center'>{description}</p>
                     <div className='flex gap-4'>
                         <button className='bg-white py-2 px-4  hover:bg-transparent hover:border hover:border-white hover:text-white text-gray-900 font-semibold'
