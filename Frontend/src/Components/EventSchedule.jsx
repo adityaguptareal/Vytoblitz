eb14086313692480970497f5b6cd87ff191a8c93
Aditya Kumar Gupta <78353347+adityaguptareal@users.noreply.github.com>
2024-12-29 23:02:19 +0530

    Fix the eventschedule component responsiveness

--- Frontend/src/Components/EventSchedule.jsx
+++ Frontend/src/Components/EventSchedule.jsx
@@ -54,15 +54,15 @@ function EventSchedule({ time, eventName, venue }) {
                     return (
                         <div key={index}>
                             <div className='flex justify-between items-center border-t border-white py-4 font-Montserrat w-3/4 mx-auto text-center'>
-                                <div className='text-base'>{eventData.time}</div>
+                                <div className='lg:text-base text-[12px]'>{eventData.time}</div>
                                 <div id="event" className='flex gap-3 justify-center items-center'>
-                                    <div className='text-lg'>{eventData.eventName}</div>
-                                    <div id="badge" className='text-slate-950 text-sm px-2 font-semibold bg-white rounded-md '>
+                                    <div className='lg:text-lg text-[12px]'>{eventData.eventName}</div>
+                                    <div id="badge" className='text-slate-950 lg:text-sm text-[12px] px-2 font-semibold bg-white rounded-md '>
                                         In Person
                                     </div>
                                 </div>
-                                <div id="Venue">{eventData.venue}</div>
-                                <button className='bg-transparent border border-white px-3 py-2'>View Details</button>
+                                <div id="Venue" className='lg:text-base text-sm'>{eventData.venue}</div>
+                                <button className='bg-transparent border lg:text-base text-sm border-white px-3 py-2'>View Details</button>
                             </div>
                         </div>
                     );
