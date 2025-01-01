c2527752eea36118718311f6d53f437ea6aeafff
Aditya Kumar Gupta <78353347+adityaguptareal@users.noreply.github.com>
2025-01-01 21:31:55 +0530

    Individual Event page routing

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -10,6 +10,11 @@ import Event from './Screens/Event';
 import About from './Screens/About'
 import Registration from './Screens/Registration'
 import Error from './Screens/Error'
+import ArtistryArena from "./Screens/ArtistryArena"
+import CulturalEvent from "./Screens/CulturalEvent"
+import Esports from "./Screens/Esports"
+import Quizathon from "./Screens/Quizathon"
+import Hackathon from "./Screens/Hackathon"
 import Footer from './Components/Footer'
 
 function App() {
@@ -19,10 +24,16 @@ function App() {
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/event' element={<Event />} />
+          <Route path='/event/artistryarena' element={<ArtistryArena />} />
+          <Route path='/event/cultural' element={<CulturalEvent />} />
+          <Route path='/event/esports' element={<Esports />} />
+          <Route path='/event/hackathon' element={<Hackathon />} />
+          <Route path='/event/quizathon' element={<Quizathon />} />
           <Route path='/about' element={<About />} />
-          <Route path='/contact' element={<Contact/>} />
+          <Route path='/contact' element={<Contact />} />
           <Route path='/registration' element={<Registration />} />
-          <Route path='*' element={<Error/>} />
+
+          <Route path='*' element={<Error />} />
         </Routes>
       </BrowserRouter>
 
