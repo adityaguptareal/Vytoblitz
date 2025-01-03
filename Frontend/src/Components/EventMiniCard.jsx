053b9a33c4a2422fe6ebbaab32c41bbe58ef025e
Aditya Kumar Gupta <78353347+adityaguptareal@users.noreply.github.com>
2025-01-03 22:15:11 +0530

    Added the Event display card component

---
+++ Frontend/src/Components/EventMiniCard.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function EventMiniCard({icon,heading,subHeading}) {
+  return (
+    <div className='flex flex-col justify-center items-start bg-gray-800 w-[200px] text-white font-Montserrat rounded-md gap-1 p-4 '>
+      <div className='text-xl'>{icon}</div>
+      <p className='text-xl font-bold'>{heading}</p>
+      <p className='text-base'>{subHeading}</p>
+    </div>
+  )
+}
+
+export default EventMiniCard
