ab018252d62b49f3adbf6e9aa35364528185c616
Aditya Kumar Gupta <78353347+adityaguptareal@users.noreply.github.com>
2025-01-04 19:48:27 +0530

    Added the link and fix styling

--- Frontend/src/Components/RectangleButton.jsx
+++ Frontend/src/Components/RectangleButton.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
+// import {useNavigate} from "react-router-dom";
 
-const Button = ({ text,rounded }) => {
+
+function RectangleButton({ text }) {
+// const navigate = useNavigate();
   return (
     <button
       className={`px-3 py-2  text-white font-normal font-Montserrat hover:scale-[1.09] transition-all bg-purple-700`}
@@ -11,4 +14,4 @@ const Button = ({ text,rounded }) => {
   );
 };
 
-export default Button;
+export default RectangleButton;
