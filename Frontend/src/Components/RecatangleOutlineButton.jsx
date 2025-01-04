fc3566e730db498fb40bf4222f06766217dcd148
Aditya Kumar Gupta <78353347+adityaguptareal@users.noreply.github.com>
2025-01-04 19:47:57 +0530

    Added the navigation and fix the button

--- Frontend/src/Components/RecatangleOutlineButton.jsx
+++ Frontend/src/Components/RecatangleOutlineButton.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import {useNavigate} from "react-router-dom";
 
-const Button = ({ text,rounded }) => {
+
+const Button = ({ text,rounded,navigateTo }) => {
+  const navigate = useNavigate();
   return (
-    <button
+    <button onClick={()=>navigate(navigateTo)}
       className={`px-6 py-2  text-white font-normal font-Montserrat hover:scale-[1.09] transition-all border-2 border-white text-center`}
       
     >
