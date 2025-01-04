0309aa6f0fbb0fe6b07a8cf08f95cb4cb84fb73c
Aditya Kumar Gupta <78353347+adityaguptareal@users.noreply.github.com>
2025-01-04 16:18:20 +0530

    Added event mini card component

--- Frontend/src/Components/EventMiniCard.jsx
+++ Frontend/src/Components/EventMiniCard.jsx
@@ -2,11 +2,12 @@ import React from 'react'
 
 function EventMiniCard({icon,heading,subHeading}) {
   return (
-    <div className='flex flex-col justify-center items-start bg-gray-800 w-[200px] text-white font-Montserrat rounded-md gap-1 p-4 '>
+    <div className='flex flex-col justify-center items-start bg-gray-800 max-w-[300px] min-w-[200px] text-white font-Montserrat rounded-md gap-1 p-4 '>
       <div className='text-xl'>{icon}</div>
       <p className='text-xl font-bold'>{heading}</p>
       <p className='text-base'>{subHeading}</p>
     </div>
+
   )
 }
 
