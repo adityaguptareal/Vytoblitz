b62d608c35dcb725dcd0c89d9d668f4d753eee80
Aditya Kumar Gupta <78353347+adityaguptareal@users.noreply.github.com>
2025-01-04 21:11:28 +0530

    fix the error page

--- Frontend/src/Screens/Error.jsx
+++ Frontend/src/Screens/Error.jsx
@@ -10,7 +10,7 @@ function Error() {
                 <div className='flex items-center overflow-hidden gap-3 flex-col justify-center h-screen'>
                     <div className='text-8xl font-bold text-white font-Orbitron text-center uppercase'>Error 404</div>
                     <div className='text-3xl text-white font-Orbitron text-center'>Resource Not Found !</div>
-                    Test
+                   
                 </div>
                 <Footer/>
             </div>
